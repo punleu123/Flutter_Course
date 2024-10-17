274006cf154df5e21799315ab1f41b2476c4e289
punleu123 <146927040+punleu123@users.noreply.github.com>
2024-10-17 13:26:50 +0700

    Update Extra-Work-Building-House.dart

--- W2-S1-LEARNING-Dart-OOP/Assignment/OOP-Beginners/Extra-Work-Building-House.dart
+++ W2-S1-LEARNING-Dart-OOP/Assignment/OOP-Beginners/Extra-Work-Building-House.dart
@@ -22,6 +22,13 @@ class Door {
   Door(this.material, this.width, this.height);
 }
 
+class Tree {
+  String type;
+  double height;
+
+  Tree(this.type, this.height);
+}
+
 class House {
   String address;
   String color;
@@ -49,25 +56,3 @@ class House {
   }
 }
 
-void main() {
-  House myHouse = House('St k5', 'Blue');
-
-  myHouse.addWindow(Window('Sliding', 1.5, 1.2));
-  myHouse.addWindow(Window('Casement', 1.0, 1.5));
-
-  myHouse.addRoof(Roof('Shingles', 120.0));
-
-  myHouse.addDoor(Door('Wood', 0.9, 2.0));
-  myHouse.addDoor(Door('Metal', 1.0, 2.1));
-
-
-  myHouse.addTree(Tree('Oak', 5.0));
-  myHouse.addTree(Tree('Pine', 6.5));
-
-  print('House Address: ${myHouse.address}');
-  print('House Color: ${myHouse.color}');
-  print('Number of Windows: ${myHouse.windows.length}');
-  print('Number of Roofs: ${myHouse.roofs.length}');
-  print('Number of Doors: ${myHouse.doors.length}');
-  print('Number of Trees: ${myHouse.trees.length}');
-}
